96d9d6db5fe7f6e6ab023ae2e2f17c27f6895ffc
gabastil <glenn.abastillas@gmail.com>
2020-03-04 14:03:24 -0500

    Added reset_ranges(), remove and update  capabilities

--- timer/js/index.js
+++ timer/js/index.js
@@ -15,6 +15,8 @@ $(document).ready(function(){
 	const canvas_width = $("svg").width();
 	const canvas_height = $("svg").height();
 
+	var n_circles;
+
 	console.log(random_between(5, 10));
 	populate_circles();
 
@@ -23,6 +25,7 @@ $(document).ready(function(){
 	// Update Labels when Range is Changed
 	movement_delay.change(update_movement_delay_label);
 	creation_delay.change(update_creation_delay_label);
+	reset.click(reset_ranges);
 
 	// Class Definitions
 
@@ -34,28 +37,46 @@ $(document).ready(function(){
 	 * @params {integer} max - maximum number of circles to draw
 	 */
 	function populate_circles(min=50, max=100){
-		let n_circles = random_between(min, max);
 		let circle_i = Math.ceil(max - min);
 		let circles = [], current, cx, cy, r;
+
+		n_circles = random_between(min, max);
 
 		while (circle_i < n_circles){
 			cx = random_between(1, canvas_width);
 			cy = random_between(1, canvas_height);
 			r = random_between(5, 15);
 
-			current = {"cx" : cx , "cy" : cy , "r" : r};
+			current = {"cx" : cx , "cy" : cy , "r" : r, "idx" : circle_i};
 			circles.push(current);
+			circle_i++;
 		}
 
 		svg.selectAll("circle")
 		   .data(circles)
 		   .enter()
 		   .append("circle")
+		   .attr("id", function(d, i){return `circle_${d.idx}`})
 		   .attr("cx", function(d, i){return d.cx})
 		   .attr("cy", function(d, i){return d.cy})
+		   .transition()
+		   .duration(movement_delay.attr("value"))
 		   .attr("r", function(d, i){return d.r})
 		   .attr("fill", "black")
 		   .attr("stroke", "#dfefff");
+	}
+
+	function update_circles(){
+		let slope = canvas_height / canvas_width;
+
+		svg.selectAll("circle")
+		   .transition()
+		   .duration(movement_delay.attr("value"))
+		   .attr("cy", function(d, i){return d.idx * i * slope})
+	}
+
+	function remove_circles(){
+		svg.selectAll("circles").remove();
 	}
 
 	/**
@@ -76,6 +97,9 @@ $(document).ready(function(){
 	function reset_ranges(){
 		creation_delay.attr("value", "2.5");
 		movement_delay.attr("value", "2.5");
+		// remove_circles();
+		update_circles();
+		// populate_circles();
 	}
 
 	/**
@@ -86,6 +110,7 @@ $(document).ready(function(){
 		let label = $("label[for='creation-delay']");
 		var base = "Creation Delay";
 		label.text(`${base} at ${this.value / 1000} s`);
+		update_circles();
 	}
 
 	/**
