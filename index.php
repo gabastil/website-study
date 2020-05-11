1ceb0071ffd64754a7cfd7a2a57be8fbbd3abace
gabastil <glenn.abastillas@gmail.com>
2020-05-11 16:09:44 -0400

    Added generic index.php in root folder

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+
+	</style>
+</head>
+<body>
+	<div id='main' class='blue-frame'>
+		<form>
+			<section name='input-text'>
+				<input type="text" name="generic-input">
+			</section>
+		</form>
+	</div>
+</body>
+</html>
